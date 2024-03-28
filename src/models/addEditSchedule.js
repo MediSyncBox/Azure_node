e4f27efb3a2de464418e452ae7926cc81a73cc57
TJWRii <s1912558@ed.ac.uk>
2024-03-28 20:23:22 +0000

    try fix 500 error

--- src/models/addEditSchedule.js
+++ src/models/addEditSchedule.js
@@ -5,31 +5,39 @@ const router = express.Router();
 
 // Function to add or update a schedule
 async function addEditSchedule(req, res) {
-    const { id, userId, medicine, dose, time, taken } = req.body;
+    const { userId, medicine, dose, time, taken } = req.body;
 
     try {
         let pool = await sql.connect(dbConfig);
-
-        if (id) {
-            // If an ID is provided, we update the existing record.
-            await pool.request()
-                .input('Id', sql.Int, id)
-                .input('UserId', sql.Int, userId)
-                .input('Medicine', sql.NVarChar, medicine)
-                .input('Dose', sql.Int, dose)
-                .input('Time', sql.SmallDateTime, new Date(time))
-                .input('Taken', sql.Bit, taken)
-                .query('UPDATE dbo.schedule SET medicine = @Medicine, dose = @Dose, time = @Time, taken = @Taken WHERE id = @Id;');
-        } else {
-            // If no ID is provided, we insert a new record.
-            await pool.request()
+        await pool.request()
                 .input('UserId', sql.Int, userId)
                 .input('Medicine', sql.NVarChar, medicine)
                 .input('Dose', sql.Int, dose)
                 .input('Time', sql.SmallDateTime, new Date(time))
                 .input('Taken', sql.Bit, taken)
                 .query('INSERT INTO dbo.schedule (user_id, medicine, dose, time, taken) VALUES (@UserId, @Medicine, @Dose, @Time, @Taken);');
-        }
+        
+
+        // if (id) {
+        //     // If an ID is provided, we update the existing record.
+        //     await pool.request()
+        //         .input('Id', sql.Int, id)
+        //         .input('UserId', sql.Int, userId)
+        //         .input('Medicine', sql.NVarChar, medicine)
+        //         .input('Dose', sql.Int, dose)
+        //         .input('Time', sql.SmallDateTime, new Date(time))
+        //         .input('Taken', sql.Bit, taken)
+        //         .query('UPDATE dbo.schedule SET medicine = @Medicine, dose = @Dose, time = @Time, taken = @Taken WHERE id = @Id;');
+        // } else {
+        //     // If no ID is provided, we insert a new record.
+        //     await pool.request()
+        //         .input('UserId', sql.Int, userId)
+        //         .input('Medicine', sql.NVarChar, medicine)
+        //         .input('Dose', sql.Int, dose)
+        //         .input('Time', sql.SmallDateTime, new Date(time))
+        //         .input('Taken', sql.Bit, taken)
+        //         .query('INSERT INTO dbo.schedule (user_id, medicine, dose, time, taken) VALUES (@UserId, @Medicine, @Dose, @Time, @Taken);');
+        // }
 
         res.json({ message: 'Schedule successfully processed.' });
 
