999e81ba2ff487cc888bb99f35790f1b1cfb1cf6
TJWRii <s1912558@ed.ac.uk>
2024-03-29 16:47:01 +0000

    fix naming

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRoutes = require('./src/models/loginRegister');
 const addBoxRoutes = require('./src/models/addBox'); 
 const addScheduleRoutes = require('./src/models/addEditSchedule'); 
 const getSchedulesRoutes = require('./src/models/getSchedules');
-const updateScheduleRoute = require('./src/models/updateSchedule');
+const updateSchedulesRoutes = require('./src/models/updateSchedules');
 
 app.use(express.json());
 app.get('/api/test', (req, res) => {
@@ -17,7 +17,7 @@ app.use('/api', userRoutes);
 app.use('/api', addBoxRoutes);
 app.use('/api', addScheduleRoutes);
 app.use('/api', getSchedulesRoutes);
-app.use('/api', updateScheduleRoute);
+app.use('/api', updateSchedulesRoutes);
 
 
 const port = process.env.PORT || 3000;
