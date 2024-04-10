56f4f4626832cc958b6857edd892240011df4b81
Xiangrui Meng <mathilda332023@outlook.com>
2024-04-10 12:25:46 +0100

    edit tankinfo

--- src/models/setTankInfo.js
+++ src/models/setTankInfo.js
@@ -4,35 +4,33 @@ const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 
 async function setTankInfo(req, res) {
-  const { boxId, tankNumber, temperature, humidity, lightValue } = req.body;
+  const { box_id, servo_id, temperature, humidity } = req.body;
 
   try {
     let pool = await sql.connect(dbConfig);
 
     // Check if a record with the same boxId and tankNumber already exists
     const existingRecord = await pool.request()
-      .input('boxId', sql.Int, boxId)
-      .input('tankNumber', sql.Int, tankNumber)
-      .query('SELECT * FROM dbo.tank WHERE box_id = @boxId AND tank_number = @tankNumber');
+      .input('box_id', sql.Int, boxId)
+      .input('servo_id', sql.Int, tankNumber)
+      .query('SELECT * FROM [dbo].[tank] WHERE box_id = @box_id AND servo_id = @servo_id');
 
     if (existingRecord.recordset.length > 0) {
       // Update the existing record
       await pool.request()
-        .input('boxId', sql.Int, boxId)
-        .input('tankNumber', sql.Int, tankNumber)
+        .input('box_id', sql.Int, boxId)
+        .input('servo_id', sql.Int, tankNumber)
         .input('temperature', sql.Float, temperature)
         .input('humidity', sql.Float, humidity)
-        .input('lightValue', sql.Int, lightValue)
-        .query('UPDATE dbo.tank SET temperature = @temperature, humidity = @humidity, light_value = @lightValue WHERE box_id = @boxId AND tank_number = @tankNumber');
+        .query('UPDATE dbo.tank SET temperature = @temperature, humidity = @humidity WHERE box_id = @box_id AND servo_id = @servo_id');
     } else {
       // Insert a new record
       await pool.request()
-        .input('boxId', sql.Int, boxId)
-        .input('tankNumber', sql.Int, tankNumber)
+        .input('box_id', sql.Int, boxId)
+        .input('servo_id', sql.Int, tankNumber)
         .input('temperature', sql.Float, temperature)
         .input('humidity', sql.Float, humidity)
-        .input('lightValue', sql.Int, lightValue)
-        .query('INSERT INTO dbo.tank (box_id, tank_number, temperature, humidity, light_value) VALUES (@boxId, @tankNumber, @temperature, @humidity, @lightValue)');
+        .query('INSERT INTO dbo.tank (box_id, servo_id, temperature, humidity) VALUES (@box_id, @servo_id, @temperature, @humidity)');
     }
 
     res.json({ message: 'Tank information updated successfully' });
