91ba08d115bb488eda7d0388dcfe34e0018f2d34
Xiangrui Meng <mathilda332023@outlook.com>
2024-04-12 14:11:07 +0100

    edit route

--- src/models/checkMedicineSchedule.js
+++ src/models/checkMedicineSchedule.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const sql = require('mssql');
-const dbConfig = require('./dbConfig');
+const dbConfig = require('../dbConfig');
 const router = express.Router();
 const cron = require('node-cron');
 
